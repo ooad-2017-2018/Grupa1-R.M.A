1f9c6744ed02d43299da4058426da3e6cd3291fd
rappabugis1 <rappabugis1@etf.unsa.ba>
2018-05-15 16:57:04 +0200

    1.Dodane .xaml forme za homeview, registraciju, mainpage, i navigaciju 2.Dodana user controla za login 3.Povecana minimalna podrzana verzija za app

--- Projekat/AirX/AirX/MainPage.xaml.cs
+++ Projekat/AirX/AirX/MainPage.xaml.cs
@@ -22,11 +22,64 @@ namespace AirX
     /// </summary>
     public sealed partial class MainPageNovi : Page
     {
+        Popup Loginpopup;
+
         public MainPageNovi()
         {
             this.InitializeComponent();
-           
+            Loginpopup = new Popup();
+
+            
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            this.ContentFrame.Navigate(typeof(HomeView));
+        }
+
+        private void OnLoginClick (object sender, RoutedEventArgs e)
+        {
+            if (!Loginpopup.IsOpen)
+            {
+                
+
+                LoginPopup login = new LoginPopup();
+
+                Loginpopup.Child= login;
+
+                Loginpopup.HorizontalOffset = (Window.Current.Bounds.Width)/2- 100;
+                Loginpopup.VerticalOffset = (Window.Current.Bounds.Height)/2 -150;
+
+
+                this.IsHitTestVisible = false;
+                this.Opacity = 0.1;
+
+                this.Loginpopup.Closed += Loginpopup_Closed;
+
+                Loginpopup.IsOpen = true;
+            }
+        }
+
+        private void Loginpopup_Closed(object sender, object e)
+        {
+            this.IsHitTestVisible = true;
+            this.Opacity = 1;
+        }
+
+        private void Navigate (object sender, RoutedEventArgs e)
+        {
+            if (sender != null)
+            {
+                NavigationViewItem button = sender as NavigationViewItem;
+
+                if (button.Tag is "home")
+                    this.ContentFrame.Navigate(typeof(HomeView));
+
+            }
+            
         }
         
+
     }
 }
